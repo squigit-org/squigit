77d2d03647454f224df08db65b61058144f4a4ed
a7mddra <a7mddra@gmail.com>
2025-10-24 22:02:38 +0300

    Create PKG YCAPTOOL

--- packages/ycaptool/src/main.cpp
+++ packages/ycaptool/src/main.cpp
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2025  a7mddra-spatialshot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
 #include <QGuiApplication>
 #include <QDebug>
 #include <QDir>
@@ -12,44 +29,54 @@
 #include "utils.h"
 #include "shell.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QGuiApplication app(argc, argv);
     QString platform = app.platformName();
     bool isWayland = platform.startsWith("wayland");
-    if (!isWayland) {
+    if (!isWayland)
+    {
         qWarning() << "This tool is for Wayland only. Exiting.";
         return 1;
     }
     qDebug() << "Detected Wayland environment.";
     QString savePath = qgetenv("SC_SAVE_PATH");
     QString xdgCache = qgetenv("XDG_CACHE_HOME");
-    if (xdgCache.isEmpty()) {
+    if (xdgCache.isEmpty())
+    {
         xdgCache = QDir::homePath() + "/.cache";
     }
-    if (savePath.isEmpty()) {
+    if (savePath.isEmpty())
+    {
         savePath = xdgCache + "/spatialshot/tmp";
     }
-    if (savePath.isEmpty()) {
+    if (savePath.isEmpty())
+    {
         qWarning() << "Error: savePath is empty.";
         return 1;
     }
     QDir homeDir = QDir::home();
-    if (savePath == "/" || savePath == homeDir.path() || savePath == homeDir.path() + "/") {
+    if (savePath == "/" || savePath == homeDir.path() || savePath == homeDir.path() + "/")
+    {
         qWarning() << "Error: Refusing to remove critical path: " << savePath;
         return 1;
     }
     QDir dir(savePath);
-    if (dir.exists()) {
-        if (!dir.removeRecursively()) {
+    if (dir.exists())
+    {
+        if (!dir.removeRecursively())
+        {
             qWarning() << "Error: Failed to remove existing save path.";
             return 1;
         }
     }
-    if (!QDir().mkpath(savePath)) {
+    if (!QDir().mkpath(savePath))
+    {
         qWarning() << "Error: Failed to create save path.";
         return 1;
     }
-    if (!QDir::setCurrent(savePath)) {
+    if (!QDir::setCurrent(savePath))
+    {
         qWarning() << "Error: Failed to set current directory to save path.";
         return 1;
     }
@@ -64,11 +91,13 @@ int main(int argc, char *argv[]) {
     QVariantMap options;
     options["interactive"] = false;
     QDBusReply<QDBusObjectPath> request_path_reply = portal.call("Screenshot", "", options);
-    if (!request_path_reply.isValid()) {
+    if (!request_path_reply.isValid())
+    {
         qWarning() << "D-Bus call failed:" << request_path_reply.error().message();
     }
     bool portalSuccess = false;
-    if (request_path_reply.isValid()) {
+    if (request_path_reply.isValid())
+    {
         QDBusObjectPath request_path = request_path_reply.value();
         qDebug() << "Got request object path:" << request_path.path();
         Receiver receiver;
@@ -79,17 +108,18 @@ int main(int argc, char *argv[]) {
             "org.freedesktop.portal.Request",
             "Response",
             &receiver,
-            SLOT(handleResponse(uint, const QVariantMap&))
-        );
-        QObject::connect(&receiver, &Receiver::finished, &loop, [&loop, &portalSuccess](bool success) {
+            SLOT(handleResponse(uint, const QVariantMap &)));
+        QObject::connect(&receiver, &Receiver::finished, &loop, [&loop, &portalSuccess](bool success)
+                         {
             portalSuccess = success;
-            loop.quit();
-        });
+            loop.quit(); });
         qDebug() << "Waiting for portal response...";
         loop.exec();
     }
-    if (!portalSuccess) {
-        if (!tryWlroots()) {
+    if (!portalSuccess)
+    {
+        if (!tryWlroots())
+        {
             qWarning() << "Both portal and wlroots fallback failed.";
             am.restore_audio();
             return 1;
--- packages/ycaptool/src/receiver.cpp
+++ packages/ycaptool/src/receiver.cpp
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) 2025  a7mddra-spatialshot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ **/
+
 #include "receiver.h"
 #include <QDebug>
 #include <QUrl>
@@ -5,25 +22,35 @@
 #include <QPixmap>
 #include "utils.h"
 
-void Receiver::handleResponse(uint response, const QVariantMap& results) {
+void Receiver::handleResponse(uint response, const QVariantMap &results)
+{
     bool ok = true;
-    if (response != 0) {
+    if (response != 0)
+    {
         qWarning() << "Portal request failed or was cancelled. Response:" << response;
         ok = false;
-    } else {
+    }
+    else
+    {
         QString uri = results.value("uri").toString();
-        if (uri.isEmpty()) {
+        if (uri.isEmpty())
+        {
             qWarning() << "Portal did not return a URI.";
             ok = false;
-        } else {
+        }
+        else
+        {
             QString fullImagePath = QUrl(uri).toLocalFile();
             qDebug() << "Full desktop image saved to:" << fullImagePath;
             QPixmap fullDesktop(fullImagePath);
             QFile::remove(fullImagePath);
-            if (fullDesktop.isNull()) {
+            if (fullDesktop.isNull())
+            {
                 qWarning() << "Failed to load pixmap from" << fullImagePath;
                 ok = false;
-            } else {
+            }
+            else
+            {
                 ok = processFullPixmap(fullDesktop);
             }
         }
