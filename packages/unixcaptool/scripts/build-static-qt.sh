0e473f97a898ba5368d36d82ae794564a7901776
a7mddra <a7mddra@gmail.com>
2025-10-26 23:25:45 +0300

    Fix Bugs

---
+++ packages/unixcaptool/scripts/build-static-qt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+QT_VERSION=6.2.4
+QT_ARCHIVE=qt-everywhere-src-${QT_VERSION}.tar.xz
+QT_SRC_DIR=qt-everywhere-src-${QT_VERSION}
+INSTALL_DIR=$(pwd)/build/.qt
+
+# Download Qt source code
+if [ ! -f "${QT_ARCHIVE}" ]; then
+    wget https://download.qt.io/archive/qt/6.2/${QT_VERSION}/single/${QT_ARCHIVE}
+fi
+
+# Extract source code
+if [ ! -d "${QT_SRC_DIR}" ]; then
+    tar -xf ${QT_ARCHIVE}
+fi
+
+# Configure Qt for static build
+cd ${QT_SRC_DIR}
+
+./configure -static -release -opensource -confirm-license \
+    -prefix ${INSTALL_DIR} \
+    -nomake examples -nomake tests \
+    -skip qtwebengine -skip qtquick3d -skip qtlottie \
+    -no-dbus -no-icu -no-opengl
+
+# Build and install Qt
+make -j$(nproc)
+make install
+
+cd ..
