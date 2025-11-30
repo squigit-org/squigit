980cfe8c098085d8bb359e4c5c0fa1bd9cc21745
a7mddra <a7mddra@gmail.com>
2025-11-30 20:23:58 +0200

    Fix installers

--- packaging/linux/scripts/install.sh
+++ packaging/linux/scripts/install.sh
@@ -8,6 +8,8 @@ set -e
 # --- Paths ---
 CACHE_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/spatialshot/cache"
 APP_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/spatialshot/app"
+APP_ICO="$APP_DIR/resources/app/assets/icons/light/512.png"
+ICO_DIR="$HOME/.icons/applications/spatialshot"
 CAPKIT_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/spatialshot/capkit"
 UNINSTALL_SCRIPT_PATH="$APP_DIR/uninstall.sh"
 
@@ -19,9 +21,10 @@ ORCHESTRATOR_URL="${RELEASES_URL}/orchestrator${EXEC_SUFFIX}"
 SPATIALSHOT_URL="${RELEASES_URL}/spatialshot${EXEC_SUFFIX}"
 
 echo "Creating directories..."
-mkdir -p "$CACHE_DIR"
 mkdir -p "$APP_DIR"
+mkdir -p "$ICO_DIR"
 mkdir -p "$CAPKIT_DIR"
+mkdir -p "$CACHE_DIR"
 echo "  > Directories created."
 
 echo "Downloading components..."
@@ -40,6 +43,8 @@ unzip -o "$CACHE_DIR/capkit.zip" -d "$CAPKIT_DIR"
 
 chmod +x "$APP_DIR/spatialshot-orchestrator"
 
+cp "$APP_ICO" "$ICO_DIR/512.png"
+
 echo "  > Files installed."
 
 echo "Setting up launchers and uninstaller..."
@@ -48,11 +53,9 @@ Version=1.0
 Type=Application
 Name=Spatialshot
 Exec=$HOME/.local/share/spatialshot/app/spatialshot --no-sandbox
-Icon=$HOME/.local/share/spatialshot/app/resources/app/assets/icons/light/512.png 
+Icon=$ICO_DIR/512.png
 Terminal=false
 Categories=Utility;" > "$HOME/.local/share/applications/spatialshot.desktop"
-
-chmod +x "${XDG_DATA_HOME:-$HOME/.local/share}/applications/spatialshot.desktop"
 
 echo "Cleaning up..."
 rm -rf "$CACHE_DIR"
