54520f14f7d1f4f1b61ab0588c417e6184f7ebb3
a7mddra <a7mddra@gmail.com>
2025-12-04 10:21:03 +0200

    Create PKG Kernel

---
+++ packages/kernel/source/main.rs
@@ -0,0 +1,8 @@
+#![windows_subsystem = "windows"]
+
+/**
+ * @license
+ * Copyright 2025 a7mddra
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
---
+++ packages/kernel/source/utilities/watchdog.rs
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * Copyright 2025 a7mddra
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
