ed9adef884371218be9a82f965ce7281038e90f2
a7mddra <a7mddra@gmail.com>
2025-12-08 04:15:54 +0200

    Refactor window positioning

--- packages/setup/src-tauri/src/lib.rs
+++ packages/setup/src-tauri/src/lib.rs
@@ -15,11 +15,15 @@ pub fn run() {
       if let Some(window) = app.get_webview_window("main") {
         if let Ok(Some(monitor)) = window.current_monitor() {
             let screen_size = monitor.size();
+            let monitor_pos = monitor.position();
             let width = (screen_size.width as f64 * (730.0 / 1366.0)).round() as u32;
             let height = (screen_size.height as f64 * (580.0 / 768.0)).round() as u32;
             
+            let x = monitor_pos.x + ((screen_size.width as i32 - width as i32) / 2);
+            let y = monitor_pos.y + ((screen_size.height as i32 - height as i32) / 2);
+
             let _ = window.set_size(tauri::Size::Physical(tauri::PhysicalSize { width, height }));
-            let _ = window.center();
+            let _ = window.set_position(tauri::Position::Physical(tauri::PhysicalPosition { x, y }));
             let _ = window.show();
         }
       }
