09fa2fe8e75c4f2ac9513f4a1b3b3d482f3e54ff
a7mddra <a7mddra@gmail.com>
2026-03-07 14:43:31 +0200

    feat(assets): remove welcome icon from assets
    - Deleted the welcome icon image file and updated the index export accordingly to streamline asset management.

--- ui/src/assets/index.ts
+++ ui/src/assets/index.ts
@@ -6,7 +6,6 @@
 
 export { AppLogo } from "./icons/AppLogo";
 export { default as updateIcon } from "./icons/emoji_u1f4e6.png";
-export { default as welcomeIcon } from "./icons/emoji_u1f6e0.png";
 
 export { default as linuxInstruction } from "./instructions/linux.md?raw";
 export { default as macosInstruction } from "./instructions/macos.md?raw";
